96ce7d252c9f65a8d138d6b3716789e92ffbbbc9
Rudy Mas <rudy.mas@rudymas.be>
2022-05-25 09:06:34 +0200

    PHP version set to 7.4

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Tiger (PHP version 7.1)
+ * Tiger (PHP version 7.4)
  * An easy to use MVC PHP Framework with Mobile App Support.
  *
  * @author      Rudy Mas <rudy.mas@rmsoft.be>
  * @copyright   2022, rmsoft.be. (http://www.rmsoft.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     7.1.1.0
+ * @version     7.4.1.0
  */
 
 use Tiger\Core;
@@ -15,5 +15,5 @@ session_start();
 require_once('vendor/autoload.php');
 
 
-const TIGER_VERSION = '7.1.1.0';
+const TIGER_VERSION = '7.4.1.0';
 $Core = new Core();
