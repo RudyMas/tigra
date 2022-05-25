3ba030142d9509639042060735ab33bf79463fae
Rudy Mas <rudy.mas@rudymas.be>
2022-05-25 07:59:03 +0200

    Framework version set to 7.1.1.0

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Tiger (PHP version 8.1)
+ * Tiger (PHP version 7.1)
  * An easy to use MVC PHP Framework with Mobile App Support.
  *
  * @author      Rudy Mas <rudy.mas@rmsoft.be>
  * @copyright   2022, rmsoft.be. (http://www.rmsoft.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     0.0.0.1
+ * @version     7.1.1.0
  */
 
 use Tiger\Core;
@@ -15,5 +15,5 @@ session_start();
 require_once('vendor/autoload.php');
 
 
-define('TIGER_VERSION', '0.0.0.1');
+define('TIGER_VERSION', '7.1.1.0');
 $Core = new Core();
